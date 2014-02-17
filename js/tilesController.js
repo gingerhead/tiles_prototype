b4912f9b3cf61498d510633d740afe8fc3be422b
marina <starostinamb@gmail.com>
2014-02-17 19:24:53 +0400

    not appropriate position

--- js/tilesController.js
+++ js/tilesController.js
@@ -27,7 +27,7 @@
       $('.turning' ).click( function() {
         var rel = $( this ).attr('rel');
         self.$tiles.removeClass('shown').addClass('hidden');
-        self.updateHidden();
+        //self.updateHidden();
         $('.tile[rel = '+rel+']').removeClass('hidden' ).addClass('shown');
         self.updateShown();
       });
@@ -35,41 +35,83 @@
 
     updateHidden: function() {
       var self = this;
-      $('.tile.hidden').each( function(i) {
-        var offset_v;
-        var offset_h;
+      $('.tile.hidden').each( function() {
         var index = $(this).index();
-        offset_h = ( index % self.tiles_in_row ) * self.col_width;
-        offset_v = Math.floor(index / self.tiles_in_row)*self.col_height;
-        $( this ).css({'top': offset_v, 'left': offset_h});
+        var offset_h = ( index % self.tiles_in_row ) * self.col_width;
+        var offset_v = Math.floor(index / self.tiles_in_row)*self.col_height;
+        self.animateObject(offset_h, offset_v, $(this));
+        //$( this ).css({'top': offset_v, 'left': offset_h});
       });
     },
 
     updateShown: function() {
-      var result = this.getCoordinates(0);
       var self = this;
-      var x = result['x'];
-      var y = result['y'];
-      //animateObject(x, y, 9);
       $('.tile.shown').each( function(i) {
-        var offset_v;
-        var offset_h;
-        var index = i;
-        offset_h = ( index % self.tiles_in_row ) * self.col_width;
-        offset_v = Math.floor(index / self.tiles_in_row)*self.col_height;
-        $( this ).css({'top': offset_v, 'left': offset_h});
+        var offset_h = ( i % self.tiles_in_row ) * self.col_width;
+        var offset_v = Math.floor( i / self.tiles_in_row) * self.col_height;
+        self.animateObject(offset_h, offset_v, $(this));
+        //$( this ).css({'top': offset_v, 'left': offset_h});
       });
     },
-
+/*
     getCoordinates: function(index) {
       var self = this;
       var result = [];
-      offset_h = ( index % self.tiles_in_row ) * self.col_width;
-      offset_v = Math.floor(index / self.tiles_in_row)*self.col_height;
-      $( this ).css({'top': offset_v, 'left': offset_h});
+      var offset_h = ( index % self.tiles_in_row ) * self.col_width;
+      var offset_v = Math.floor(index / self.tiles_in_row)*self.col_height;
+      //$( this ).css({'top': offset_v, 'left': offset_h});
       result['x'] = offset_h;
       result['y'] = offset_v;
       return result;
+    },*/
+
+    animateObject: function(x1,y1,$elem) {
+      var x0 = parseInt($elem.css('left' ), 10);
+      var y0 = parseInt($elem.css('top' ), 10);
+      var koef_x = ((x1-x0)/Math.abs(x1-x0));
+      var koef_y = ((y1-y0)/Math.abs(y1-y0));
+      this.moveHorizontal($elem,x1,y1,koef_x,koef_y);
+    },
+
+    moveHorizontal: function($elem,x1,y1,koef_x,koef_y) {
+      console.log($elem.index(),x1,y1,koef_x,koef_y);
+      var self = this;
+      var current_left = parseInt($elem.css('left' ), 10);
+      var current_top = parseInt($elem.css('top' ), 10);
+      if ( current_left > x1 ) {
+        $elem.animate( { left: "+="+(koef_x*self.col_width)+"px"  }, {
+          duration: 200,
+          step: function(now) {
+            current_left = now;
+          },
+          complete: function() {
+            self.moveVertical($elem,x1,y1,koef_x,koef_y);
+          }
+        });
+      }
+      else if (current_top > y1) {
+        this.moveVertical($elem,x1,y1,koef_x,koef_y);
+      }
+    },
+
+    moveVertical: function($elem,x1,y1,koef_x,koef_y) {
+      var self = this;
+      var current_top = parseInt($elem.css('top' ), 10);
+      var current_left = parseInt($elem.css('left' ), 10);
+      if ( current_top > y1 ) {
+        $elem.animate( { top: "+="+(self.col_height*koef_y)+"px" }, {
+          duration: 200,
+          step: function(now) {
+            current_top = now;
+          },
+          complete: function() {
+            self.moveHorizontal($elem,x1,y1,koef_x,koef_y)
+          }
+        });
+      }
+      else if (current_left > x1) {
+        this.moveHorizontal($elem,x1,y1,koef_x,koef_y);
+      }
     }
 
   }
